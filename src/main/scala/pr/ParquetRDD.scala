7b6e7f6b27577cc0784c57b9003b22b7d0f7e1f3
Pawel Szulc <paul.szulc@gmail.com>
2017-01-26 15:37:46 +0100

    Implement getPartitions

--- src/main/scala/pr/ParquetRDD.scala
+++ src/main/scala/pr/ParquetRDD.scala
@@ -1,20 +1,40 @@
 package pr
 
+import org.apache.hadoop.fs.Path
+import org.apache.hadoop.mapred.FileSplit
+
 import org.apache.spark._
 import org.apache.spark.rdd._
+
 import scala.reflect.ClassTag
 
-class ParquetRDD[T : ClassTag](_sc: SparkContext) extends RDD[T](_sc, Nil) {
+class ParquetRDDPartition(val index: Int, split: FileSplit) extends Partition {
+
+}
+
+class ParquetRDD[T : ClassTag](
+  _sc: SparkContext,
+  path: Path
+) extends RDD[T](_sc, Nil) {
 
   override def compute(split: Partition, context: TaskContext): Iterator[T] = ???
   
-  override protected def getPartitions: Array[Partition] = ???
+  override protected def getPartitions: Array[Partition] = {
+    val fs = path.getFileSystem(_sc.hadoopConfiguration)
+    val fileStatus = fs.getFileStatus(path)
+    val blocks = fs.getFileBlockLocations(fileStatus, 0, fileStatus.getLen())
+    blocks.zipWithIndex.map {
+      case (b, i) =>
+        val split = new FileSplit(path,b.getOffset(),b.getLength(),b.getHosts())
+        new ParquetRDDPartition(i, split)
+    }
+  }
 
 }
 
 object ParquetRDD {
   implicit class SparkContextOps(sc: SparkContext) {
-    def parquet[T : ClassTag]: ParquetRDD[T] =
-      new ParquetRDD[T](sc)
+    def parquet[T : ClassTag](path: String): ParquetRDD[T] =
+      new ParquetRDD[T](sc, new Path(path))
   }
 }
